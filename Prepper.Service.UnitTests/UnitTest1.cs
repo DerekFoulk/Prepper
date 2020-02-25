e306e71f23219c80c1c7368d5b044f3c50473873
Derek Foulk <derekfoulk@gmail.com>
2020-02-25 01:43:36 -0800

    Added Prepper.Service.UnitTests.csproj

---
+++ Prepper.Service.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Prepper.Service.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
